678fc5a72397cb2eec4702380f28a22d87a59525
kevin flessa <kfless12@gmail.com>
2021-01-15 16:56:37 -0500

    Finished assignment

--- family-tree.js
+++ family-tree.js
@@ -3,15 +3,56 @@ class FamilyTree {
     if(typeof name === 'string'){
     this.value = name
     } else {throw 'error'};
+    this.children = []
   }
-  familySize(){}
+  familySize(){
+    return this.children.length + 1
+  }
   findMember(name){
- if(true){return node}
-  return undefined
+    for (let i = 0; i < this.children.length; i++){
+      if (this.children[i].value === name){
+        return this.children[i]
+      }
+    }
+      return undefined
+    }
+   
+  log(){
+    let list = [`-- ${this.value}`]
+    this.children.map(function (x){
+      list.push(`---- ${x.value}`)
+      x.children.map(function(x){
+      list.push(`------ ${x.value}`)
+      })})    
+let tot = [list.join('\n')]
+    return tot
   }
-  log(){}
-  insert(){}
+  insert(name){
+    return this.children.push(new FamilyTree(name))
+  }
 
 }
+const szwajkowskis = new FamilyTree('Pop');
+
+szwajkowskis.insert('Mike');
+szwajkowskis.insert('Amy');
+szwajkowskis.insert('Todd');
+
+const mikesFamily = szwajkowskis.findMember('Mike');
+
+mikesFamily.insert('Eliot');
+mikesFamily.insert('Elise');
+mikesFamily.insert('Cas');
+mikesFamily.insert('George');
+mikesFamily.insert('Lear');
+
+const amysFamily = szwajkowskis.findMember('Amy');
+
+amysFamily.insert('Henry');
+amysFamily.insert('Vivian');
+
+const log = szwajkowskis.log();
+
+console.log(log)
 
 module.exports = FamilyTree;
